9513b82645140815c572b7feaf28cca57057884f
Vinicius Ferreira <viniciuferreira32@gmail.com>
2022-10-10 23:08:36 -0300

    feat(header): created and finished Header

---
+++ src/styles/global.ts
@@ -0,0 +1,18 @@
+import { createGlobalStyle } from 'styled-components'
+
+export const GlobalStyles = createGlobalStyle`
+*{
+  margin:0;
+  padding: 0;
+  box-sizing: border-box;
+}
+
+:focus{
+  outline: 0;
+  box-shadow: 0 0 0 2px ${(props) => props.theme['gray-500']};
+}
+
+body{
+  background: ${(props) => props.theme['gray-800']};
+}
+`
