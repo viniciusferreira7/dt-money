358185cb635273ac6c5cd4ab97bd5c1ed4884861
Vinicius Ferreira <viniciuferreira32@gmail.com>
2022-10-12 20:40:49 -0300

    style(transactions): created component TransactionTable

--- src/pages/Transactions/index.tsx
+++ src/pages/Transactions/index.tsx
@@ -1,11 +1,38 @@
 import { Header } from '../../components/Header'
 import { Summary } from '../../components/Summary'
+import {
+  PriceHighLight,
+  TransactionsContainer,
+  TransactionsTable,
+} from './styles'
 
 export function Transactions() {
   return (
     <div>
       <Header />
       <Summary />
+      <TransactionsContainer>
+        <TransactionsTable>
+          <tbody>
+            <tr>
+              <td width="50%">Desenvolvimentos de site</td>
+              <td>
+                <PriceHighLight variant="income">R$ 12.000,00</PriceHighLight>
+              </td>
+              <td>Venda</td>
+              <td>13/04/2022</td>
+            </tr>
+            <tr>
+              <td width="50%">hamburger</td>
+              <td>
+                <PriceHighLight variant="outcome">- R$ 59,00</PriceHighLight>
+              </td>
+              <td>Alimentação</td>
+              <td>10/04/2022</td>
+            </tr>
+          </tbody>
+        </TransactionsTable>
+      </TransactionsContainer>
     </div>
   )
 }
