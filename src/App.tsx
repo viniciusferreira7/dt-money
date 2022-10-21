df4b15ccc372642533c05bdc7ae93f302b292ab8
Vinicius Ferreira <viniciuferreira32@gmail.com>
2022-10-20 23:00:55 -0300

    feat(transactionscontext): creating the transaction context
    [[

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,16 @@
 import { ThemeProvider } from 'styled-components'
+import { TransactionsProvider } from './contexts/TransactionsContext'
 import { Transactions } from './pages/Transactions'
 import { GlobalStyles } from './styles/global'
 import { defaultTheme } from './styles/theme/default'
 
 export function App() {
   return (
-    <ThemeProvider theme={defaultTheme}>
-      <Transactions />
-      <GlobalStyles />
-    </ThemeProvider>
+    <TransactionsProvider>
+      <ThemeProvider theme={defaultTheme}>
+        <Transactions />
+        <GlobalStyles />
+      </ThemeProvider>
+    </TransactionsProvider>
   )
 }
